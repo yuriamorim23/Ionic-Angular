399cdc37af67e0caaeabd033d9e0e33e708482e0
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-02-16 18:29:04 +0000

    interceptors para token

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,5 +1,5 @@
 //API PARA CLIENTEDTO CRIACAO DO BE PARA CHAMAR A CLASS
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
@@ -15,12 +15,8 @@ export class ClienteService {
 
    findByEmail(email: string) : Observable<ClienteDTO> {
 
-    let token = this.storage.getLocalUser().token;
-    let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
-
-    return this.http.get<ClienteDTO>(
-        `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-        {'headers': authHeader});
+    return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    
     }
 
     getImageFromBucket(id : string) : Observable<any> {
