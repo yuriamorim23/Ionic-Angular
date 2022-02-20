6fb6e97dd281d4ce1da56be618e9b3bb7761075a
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-02-20 15:31:40 +0000

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,12 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+
+    this.loadData();
+  }
+
+  loadData() {
+
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email) { 
       this.clienteService.findByEmail(localUser.email)
@@ -68,6 +74,20 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,12 +4,16 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
 import { ClienteDTO } from "../../models/cliente.dto";
+import { ImageUtilService } from "../image-util.service";
 import { StorageService } from "../storage.service";
 
 @Injectable()
 export class ClienteService {
 
-   constructor (public http : HttpClient, public storage : StorageService) {
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imageUtilService: ImageUtilService) {
 
    }
 
@@ -37,4 +41,19 @@ export class ClienteService {
             }
         ); 
     }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`, 
+            formData,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+
 }
