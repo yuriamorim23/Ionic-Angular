5ab8548137e598cee1deebb63a30addb0bd9e26c
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-02-20 14:24:06 +0000

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -23,6 +23,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -48,6 +52,7 @@ export class ProdutosPage {
   }  
 
   showDetail(produto_id : string) {
+    
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
 
@@ -57,6 +62,13 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 
 }
